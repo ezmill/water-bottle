001932b37867a80bc2fc70242a5d320260ad08b3
Ezra Miller <emiller@lsoc.org>
2015-02-15 16:12:57 -0600

    ping ponging but idk

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ var loader;
 var w = window.innerWidth;
 var h = window.innerHeight;
 var sceneRTT, cameraRTT;
+var sceneFB;
+var rtTexture, rtFB, rtFinal;
 initScene();
 function initScene(){
 	container = document.createElement('div');
@@ -15,6 +17,7 @@ function initScene(){
 	cameraRTT.position.z = 100;
     scene = new THREE.Scene();
 	sceneRTT = new THREE.Scene();
+	sceneFB = new THREE.Scene();
 
 	renderer = new THREE.WebGLRenderer();
     renderer.setSize(w, h);
@@ -26,46 +29,96 @@ function initScene(){
     rtTexture.magFilter = THREE.NearestFilter;
     rtTexture.format = THREE.RGBFormat;
 
+    rtFB = new THREE.WebGLRenderTarget(w, h);
+    rtFB.minFilter = THREE.LinearFilter;
+    rtFB.magFilter = THREE.NearestFilter;
+    rtFB.format = THREE.RGBFormat;
+
+    rtFinal = new THREE.WebGLRenderTarget(w, h);
+    rtFinal.minFilter = THREE.LinearFilter;
+    rtFinal.magFilter = THREE.NearestFilter;
+    rtFinal.format = THREE.RGBFormat;
+
     // document.addEventListener('mousemove', onDocumentMouseMove, false);
     // window.addEventListener('resize', onWindowResize, false);
+	planeGeometry = new THREE.PlaneBufferGeometry( w,h );
+
     createBackgroundScene();
+    createFeedbackScene();
     loadBottle();
 	animate();
 
 }
 function createBackgroundScene(){
-	var planeGeometry = new THREE.PlaneBufferGeometry( w,h );
+	tex = THREE.ImageUtils.loadTexture("textures/1.jpg");
 	var materialParameters = {
-		uniforms: { time: { type: "f", value: 0.0 } },
+		uniforms: { 
+			time: { type: "f", value: 0.0 } ,
+			texture: { type: "t", value: tex } 
+		},
 		vertexShader: document.getElementById( 'vs' ).textContent,
 		fragmentShader: document.getElementById( 'fs' ).textContent
 	}
-	var material = new THREE.ShaderMaterial( materialParameters );
-	var quad = new THREE.Mesh( planeGeometry, material );
+	material = new THREE.ShaderMaterial( materialParameters );
+	quad = new THREE.Mesh( planeGeometry, material );
 	sceneRTT.add( quad );
+}
+function createFeedbackScene(){
+	var tex = THREE.ImageUtils.loadTexture("textures/1.jpg");
+	var materialFBParameters = {
+		uniforms: { 
+			time: { type: "f", value: 0.0 } ,
+			texture: { type: "t", value: rtTexture } 
+		},
+		vertexShader: document.getElementById( 'vs' ).textContent,
+		fragmentShader: document.getElementById( 'fs-2' ).textContent
+	}
+	materialFB = new THREE.ShaderMaterial( materialFBParameters );
+	quad = new THREE.Mesh( planeGeometry, materialFB );
+	sceneFB.add( quad );
 }
 
 function loadBottle(){
     loader = new THREE.BinaryLoader(true);
-    var material = new THREE.MeshBasicMaterial({
+    bottleMaterial = new THREE.MeshBasicMaterial({
 		color: 0xffffff,
-		map: rtTexture
+		map: rtFinal
     })
     loader.load("js/models/water-bottle.js", function(geometry) {
-        createBottle(geometry, material);
+        createBottle(geometry, bottleMaterial);
     });
 }
 function createBottle(geometry, material){
 	var bottle = new THREE.Mesh(geometry, material);
-	var scale = 500.0;
-	bottle.position.set(0,-50,-100);
+	var scale = 1000.0;
+	bottle.position.set(0,-100,-100);
 	bottle.scale.set(scale,scale,scale);
 	scene.add(bottle);
 }
+var inc = 0;
+var addFrames = true;
 function render(){
     camera.lookAt(scene.position);
-    renderer.render( sceneRTT, camera, rtTexture, true );
+
+    // bottleMaterial.map = rtFB;
+    // inc++
+	// if(inc >= 10){
+		// addFrames = false;
+	// }
+	// if(addFrames){
+	    renderer.render(sceneRTT, cameraRTT, rtFB, true );
+	// }
+	renderer.render(sceneRTT, cameraRTT, rtTexture, true);
+	renderer.render(sceneFB, cameraRTT, rtFinal, true);
     renderer.render(scene, camera);
+
+    var a = rtFB;
+    rtFB = rtTexture;
+    rtTexture = rtFinal;
+    rtFinal = a;
+
+    // material.uniforms.texture.value = rtTexture;
+    // materialFB.uniforms.texture.value = rtFinal;
 }
 function animate(){
 	window.requestAnimationFrame(animate);
